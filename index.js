2d7fe94ecb8bfbb518e70a6092ae75bb5382073d
Anthony <obrienanthony13@gmail.com>
2017-05-14 13:17:29 +0100

    More trailers + Credit given

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ http.createServer(function (req, res) {
         else if (req.url == "/navbar") {
             webpages.Navbar(req, res);
         }
+        else if (req.url.substring(0, 13) == "/argostrailer") {
+            webpages.ArgosTrailer(req, res);
+        }
+        else if (req.url.substring(0, 18) == "/sainsburystrailer") {
+            webpages.SainsburysTrailer(req, res);
+        }
+        else if (req.url.substring(0, 16) == "/waitrosetrailer") {
+            webpages.waitroseTrailer(req, res);
+        }
         else {
             webpages.NotFound(req, res);
         }
--- webpages.js
+++ webpages.js
@@ -13,7 +13,7 @@ exports.Styles = function (req, res) {
 exports.TescoTrailer = function (req, res) {
     var file = __dirname + '/assets/tescotrailer.scs';
 
-    res.setHeader('Content-disposition', 'attachment; filename=trailer.scs');
+    res.setHeader('Content-disposition', 'attachment; filename=trailer_by_josh.scs');
 
     var filestream = fs.createReadStream(file);
     filestream.pipe(res);
@@ -32,4 +32,31 @@ exports.Trailers = function (req, res) {
 exports.Navbar = function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     fs.createReadStream('./assets/navbar.html').pipe(res);
+}
+
+exports.ArgosTrailer = function (req, res) {
+    var file = __dirname + '/assets/argostrailer.scs';
+
+    res.setHeader('Content-disposition', 'attachment; filename=trailer_by_josh.scs');
+
+    var filestream = fs.createReadStream(file);
+    filestream.pipe(res);
+}
+
+exports.SainsburysTrailer = function (req, res) {
+    var file = __dirname + '/assets/sainsburystrailer.scs';
+
+    res.setHeader('Content-disposition', 'attachment; filename=trailer_by_josh.scs');
+
+    var filestream = fs.createReadStream(file);
+    filestream.pipe(res);
+}
+
+exports.waitroseTrailer = function (req, res) {
+    var file = __dirname + '/assets/waitrosetrailer.scs';
+
+    res.setHeader('Content-disposition', 'attachment; filename=trailer_by_josh.scs');
+
+    var filestream = fs.createReadStream(file);
+    filestream.pipe(res);
 }
